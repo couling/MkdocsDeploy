550db8f52585167381ebaaabbf0a7a12c7bcc2f8
Philip Couling <couling@gmail.com>
2024-01-03 00:40:46 +0000

    Simplified mock

--- source/tests/mock_wrapper.py
+++ source/tests/mock_wrapper.py
@@ -1,5 +1,4 @@
 from typing import Any, NamedTuple, TypeVar
-from copy import deepcopy
 
 _T = TypeVar("_T")
 
--- source/tests/test_modules/test_actions.py
+++ source/tests/test_modules/test_actions.py
@@ -1,46 +1,29 @@
 import uuid
-from pathlib import Path
 
 import pytest
 
-from mkdocs_deploy import abstract, actions, versions
+from mkdocs_deploy import actions, versions
+from ..mock_plugin import MockSource, MockTargetSession
 from ..mock_wrapper import mock_wrapper
 
-
-@pytest.fixture()
-def mock_source_files(mock_source: abstract.Source, mock_source_path: Path) -> dict[str, bytes]:
-    (mock_source_path / "index.html").write_text(str(uuid.uuid4()))
-    (mock_source_path / "subdir").mkdir(exist_ok=False)
-    (mock_source_path / "subdir" / "foo.txt").write_text(str(uuid.uuid4()))
-    results = {}
-    for file in mock_source.iter_files():
-        with mock_source.open_file_for_read(file) as file_handle:
-            results[file] = file_handle.read()
-    return results
+MOCK_SOURCE_FILES = {
+    "index.html": str(uuid.uuid4()).encode(),
+    "subdir/foo.txt": str(uuid.uuid4()).encode(),
+}
 
 
 @pytest.mark.parametrize("title", ["Version 1.1", None], ids=["Explicit Title", "Implicit Title"])
-def test_upload(
-    mock_source: abstract.Source, mock_target: abstract.Target, mock_source_files: dict[str, bytes], title: str | None
-):
+def test_upload(title: str | None):
     VERSION = "1.1"
-    session, session_method_calls = mock_wrapper(mock_target.start_session())
+    source = MockSource(MOCK_SOURCE_FILES)
+    session, session_method_calls = mock_wrapper(MockTargetSession())
+
     try:
-        actions.upload(
-            source=mock_source,
-            target=session,
-            version_id=VERSION,
-            title=title,
-        )
+        actions.upload(source=source, target=session, version_id=VERSION, title=title)
 
         assert session_method_calls[0].name == "start_version"
 
-        uploaded_files = set(session.iter_files(VERSION))
-        assert uploaded_files == set(mock_source_files.keys())
-        for file in mock_source_files.keys():
-            with session.download_file(VERSION, file) as file_handle:
-                assert file_handle.read() == mock_source_files[file]
-
+        assert session.files == {(VERSION, file): content for file, content in source.files.items()}
 
         assert session.deployment_spec.versions == {
             VERSION: versions.DeploymentVersion(title=title or VERSION),
@@ -52,64 +35,28 @@ def test_upload(
         session.close(success=True)
 
 
-def test_upload_implicit_title_does_not_override_existing_one(
-    mock_source: abstract.Source, mock_target: abstract.Target, mock_source_files: dict[str, bytes]
-):
+def test_upload_implicit_title_does_not_override_existing_one():
     VERSION = "1.1"
-    session = mock_target.start_session()
-    try:
-        session._deployment_spec.versions[VERSION] = versions.DeploymentVersion(title="foo bar") # type: ignore
-        actions.upload(
-            source=mock_source,
-            target=session,
-            version_id=VERSION,
-            title=None,
-        )
+    source = MockSource(MOCK_SOURCE_FILES)
+    session, session_method_calls = mock_wrapper(MockTargetSession())
+    session.start_version(VERSION, "foo bar")
 
-        uploaded_files = set(session.iter_files(VERSION))
-        assert uploaded_files == set(mock_source_files.keys())
-        for file in mock_source_files.keys():
-            with session.download_file(VERSION, file) as file_handle:
-                assert file_handle.read() == mock_source_files[file]
+    actions.upload(source=source, target=session, version_id=VERSION, title=None)
+
+    assert session.deployment_spec.versions == {
+        VERSION: versions.DeploymentVersion(title="foo bar"),
+    }
 
 
-        assert session.deployment_spec.versions == {
-            VERSION: versions.DeploymentVersion(title="foo bar" or VERSION),
-        }
-    except:
-        session.close(success=False)
-        raise
-    else:
-        session.close(success=True)
-
-
-def test_upload_explicit_title_overrides_existing_one(
-    mock_source: abstract.Source, mock_target: abstract.Target, mock_source_files: dict[str, bytes]
-):
+def test_upload_explicit_title_overrides_existing_one():
     VERSION = "1.1"
     VERSION_TITLE = "Version 1.1"
-    session = mock_target.start_session()
-    try:
-        session._deployment_spec.versions[VERSION] = versions.DeploymentVersion(title="foo bar") # type: ignore
-        actions.upload(
-            source=mock_source,
-            target=session,
-            version_id=VERSION,
-            title=VERSION_TITLE,
-        )
+    source = MockSource(MOCK_SOURCE_FILES)
+    session, session_method_calls = mock_wrapper(MockTargetSession())
+    session.start_version(VERSION, "foo bar")
 
-        uploaded_files = set(session.iter_files(VERSION))
-        assert uploaded_files == set(mock_source_files.keys())
-        for file in mock_source_files.keys():
-            with session.download_file(VERSION, file) as file_handle:
-                assert file_handle.read() == mock_source_files[file]
+    actions.upload(source=source, target=session, version_id=VERSION, title=VERSION_TITLE)
 
-
-        assert session.deployment_spec.versions == {
-            VERSION: versions.DeploymentVersion(title=VERSION_TITLE or VERSION),
-        }
-    except:
-        session.close(success=False)
-        raise
-    else:
-        session.close(success=True)
+    assert session.deployment_spec.versions == {
+        VERSION: versions.DeploymentVersion(title=VERSION_TITLE),
+    }
